b779f50172094c65b950118f520c543fc6627f01
Shahim Essaid <shahim@essaid.com>
2021-01-22 21:08:29 -0800

    .

---
+++ jekyll-m_tool/lib/jekyll/m_tool.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "m_tool/version"
+
+module Jekyll
+  module MTool
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ jekyll-m_tool/lib/jekyll/m_tool/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Jekyll
+  module MTool
+    VERSION = "0.0.0.alpha.1"
+  end
+end
